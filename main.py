a69694c4f4a5857aeba2149e12dcda6845bfddc2
superstar306 <superelitedev@gmail.com>
2024-11-30 07:37:43 +1000

    fast API (crud)

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-def index():
-    return 'Hey devstar'
+
+@app.get('/blog')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    #only get 10 published blogs
+    if published:
+        return {'data': {'published blogs': {limit}}}
+    else:
+        return {'data': {'unpublished blogs': {limit}}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+@app.get('/about')
+def about():
+    return {'data': 'about page'}
+
+@app.get('/blog/{id}')
+def getBlog(id: int):
+    # fetch blog with id = id
+    return {'data': id}
 
 
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data': {'1', '2'}}
